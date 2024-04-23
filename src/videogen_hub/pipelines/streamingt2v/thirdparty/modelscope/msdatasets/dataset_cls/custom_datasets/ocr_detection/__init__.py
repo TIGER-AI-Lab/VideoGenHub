47297e13de56f4ee99bc3fe91b968b1c8c782194
Richard5678 <frqrichard@gmail.com>
2024-04-23 17:08:41 -0400

    removed thirdparty folder

--- src/videogen_hub/pipelines/streamingt2v/thirdparty/modelscope/msdatasets/dataset_cls/custom_datasets/ocr_detection/__init__.py
+++
@@ -1,5 +0,0 @@
-# These codes are copied from modelscope revision c58451baead80d83281f063d12fb377fad415257 
-# Copyright (c) Alibaba, Inc. and its affiliates.
-from .data_loader import DataLoader
-from .image_dataset import ImageDataset
-from .measures import QuadMeasurer
--- src/videogen_hub/pipelines/streamingt2v/thirdparty/modelscope/trainers/multi_modal/cones2/__init__.py
+++
@@ -1,3 +0,0 @@
-# These codes are copied from modelscope revision c58451baead80d83281f063d12fb377fad415257 
-# Copyright © Alibaba, Inc. and its affiliates.
-from .cones_trainer import ConesDiffusionTrainer
