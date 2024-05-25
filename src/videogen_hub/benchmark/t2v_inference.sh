eeaa4cf80b5e2eb211d5be5f92751a6a008d1d2e
richard <frqrichard@gmail.com>
2024-05-25 15:11:17 -0400

    added prompt sampling pipeline and integrate model inference pipeline

---
+++ src/videogen_hub/benchmark/t2v_inference.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Check if MLLM and device parameters are provided
+if [ "$#" -lt 2 ]; then
+    echo "Usage: $0 <model> <device>"
+    exit 1
+fi
+
+# The first command line argument is the model, the second is the device
+model="$1"
+device="$2"
+
+CUDA_VISIBLE_DEVICES="$device" python3 /home/richard/VideoGenHub/src/videogen_hub/benchmark/text_guided_t2v.py --model_name "$model"
