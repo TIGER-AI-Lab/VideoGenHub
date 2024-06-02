6552dfa935c2ddeaf6ef3ea164e4b62bbb029813
Max Ku <m3ku@uwaterloo.ca>
2024-06-02 13:28:40 -0400

    update

--- src/videogen_hub/pipelines/t2v_turbo/__init__.py
+++ src/videogen_hub/pipelines/t2v_turbo/__init__.py
@@ -1,3 +1,4 @@
 import sys
 sys.path.insert(0, './src/videogen_hub/pipelines/')
 sys.path.insert(0, './src/videogen_hub/pipelines/t2v_turbo/')
+sys.path.insert(0, './src/videogen_hub/pipelines/t2v_turbo/lvdm/')
