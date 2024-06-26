3f44b331b811f4ff6af6faa7e876bc264baf5a00
d8ahazard <d8ahazard@gmail.com>
2024-06-26 18:34:29 -0500

    Code cleanup, add SVD (Untested yet)

--- src/videogen_hub/__init__.py
+++ src/videogen_hub/__init__.py
@@ -19,17 +19,22 @@ from videogen_hub.infermodels import load, get_model, load_model
 def all_text2video_models():
     # Find all models which inherit from BaseT2vInferModel in the infermodels directory
     from videogen_hub import infermodels
-    all_infer_models = [cls for name, cls in inspect.getmembers(infermodels, inspect.isclass) if issubclass(cls, BaseT2vInferModel)]
+    all_infer_models = [cls for name, cls in inspect.getmembers(infermodels, inspect.isclass) if
+                        issubclass(cls, BaseT2vInferModel)]
     return all_infer_models
+
 
 def all_image2video_models():
     # Find all models which inherit from BaseT2vInferModel in the infermodels directory
     from videogen_hub import infermodels
-    all_infer_models = [cls for name, cls in inspect.getmembers(infermodels, inspect.isclass) if issubclass(cls, BaseT2vInferModel)]
+    all_infer_models = [cls for name, cls in inspect.getmembers(infermodels, inspect.isclass) if
+                        issubclass(cls, BaseT2vInferModel)]
     return all_infer_models
+
 
 def all_models():
     # Find all models which inherit from BaseInferModel in the infermodels directory
     from videogen_hub import infermodels
-    all_infer_models = [cls for name, cls in inspect.getmembers(infermodels, inspect.isclass) if issubclass(cls, BaseInferModel)]
+    all_infer_models = [cls for name, cls in inspect.getmembers(infermodels, inspect.isclass) if
+                        issubclass(cls, BaseInferModel)]
     return all_infer_models
