69da209f58069bf45915c09b41d288e7d9cf9b6c
Max Ku <m3ku@uwaterloo.ca>
2024-08-17 10:01:38 -0400

    Fixing lavie path

--- src/videogen_hub/infermodels/lavie.py
+++ src/videogen_hub/infermodels/lavie.py
@@ -26,9 +26,9 @@ class LaVie():
         from omegaconf import OmegaConf
 
         snapshot_download(repo_id="Vchitect/LaVie", local_dir=model_path)
-        snapshot_download(repo_id="CompVis/stable-diffusion-v1-4", local_dir=os.path.join(model_path, "/stable-diffusion-v1-4"))
+        snapshot_download(repo_id="CompVis/stable-diffusion-v1-4", local_dir=os.path.join(model_path, "stable-diffusion-v1-4"))
         snapshot_download(repo_id="stabilityai/stable-diffusion-x4-upscaler",
-                          local_dir=os.path.join(model_path, "/stable-diffusion-x4-upscaler"))
+                          local_dir=os.path.join(model_path, "stable-diffusion-x4-upscaler"))
 
         torch.set_grad_enabled(False)
         self.device = device
